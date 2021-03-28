5764b7494c9dbb08541e057c6586ef17d1ba1aec
g-aleprojetos <g.aleprojetos@gmail.com>
2021-03-28 20:11:01 -0300

    Update dispositivo-esp8266 - analógico
     *  Interrupção,
     *  ArduinoOta,
     *  Configuração de rede,
     *  fixar Ip
     *  fixar Ip,
     *  analógico

--- esp8266/dispositivo_esp8266/dispositivo_esp8266.ino
+++ esp8266/dispositivo_esp8266/dispositivo_esp8266.ino
@@ -8,7 +8,8 @@
  *  Interrupção,
  *  ArduinoOta,
  *  Configuração de rede,
- *  fixar Ip
+ *  fixar Ip,
+ *  analógico
  */
 
 // --- Bibliotecas Auxiliares ---
@@ -35,13 +36,26 @@ WiFiServer server(80); //porta de internet a ser aberta
 // --- Protótipo das Funções ---
 void startOTA();
 void ICACHE_RAM_ATTR onTimerISR();
+long moving_average(int sig);
+void temp();
 
 /************************************************************************************/
+// --- Constantes Auxiliares ---
+#define     num    100         //número de iterações da média móvel
+
 // --- Variáveis Globais ---
 const char* ssid = STASSID;
 const char* password = STAPSK;
-char counter  = 0x00;                  //variável auxiliar de contagem
-
+int sensorAnalogico = A0;
+int values[num];             //vetor com num posições, armazena os valores para cálculo da média móvel
+int adc_noise1;              //armazena a leitura sem filtro da entrada analógica
+int adc_noise2;              //armazena a leitura filtrada da entrada analógica
+int offset = 40;
+int valorTemp = 0;
+float temperature = 0.0;       //Armazena a temperatura em graus Celsius
+float current_temp = 0.0;      //Armazena a temperatura atual
+char counter1  = 0x00;          //variável auxiliar de contagem
+char counter2 = 0x00;
 /************************************************************************************/
 
 void setup() {
@@ -80,6 +94,7 @@ void setup() {
 
 void loop() {
   ArduinoOTA.handle();
+   
 }
 
 // --- Desenvolvimento das Funções ---
@@ -87,14 +102,23 @@ void loop() {
 void ICACHE_RAM_ATTR onTimerISR(){
   
       timer1_write(100000);
-      counter++;                             // incrementa counter
+      counter1++;                             // incrementa counter1
+      counter2++;                             // incrementa counter1
   
-    if(counter == 0x03)                      // counter 
+    if(counter1 == 0x20)                      // counter 
       {                 // sim...
        digitalWrite(out, !digitalRead(out)); //Inverte o estado da saída de teste
-        counter = 0x00;                      //zera o counter
+        counter1 = 0x00;                      //zera o counter
        
        } //end if counter 
+
+     if(counter2 == 0x05)                      // counter 
+      {                                        // sim...
+       temp();
+        counter2 = 0x00;                       //zera o counter
+       
+       } //end if counter
+       
 }
 
 // --- Start OTA ---
@@ -139,3 +163,44 @@ void startOTA(){
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
  }//end startOTA
+
+ long moving_average(int sig)
+{
+  int i;               //variável auxiliar para iterações
+  long acc = 0;        //acumulador
+  
+  //Desloca o vetor completamente eliminando o valor mais antigo
+  for(i = num; i > 0; i--) values[i] = values[i-1];    
+  
+  values[0] = sig;           //carrega o sinal no primeiro elemento do vetor
+  
+   
+  for(i = 0; i < num; i++) acc += values[i];
+   
+  
+  return acc / num;         //Retorna a média móvel
+
+} //end moving_average
+
+
+void temp()
+{
+adc_noise1 = analogRead(sensorAnalogico);
+adc_noise2 = moving_average(adc_noise1);
+temperature = ((adc_noise2 * 3.3)/1024.0);
+temperature = temperature * 100;
+temperature = temperature - offset;
+
+  if(temperature < (current_temp - 1.0) || temperature > (current_temp + 1.0))
+  {
+      current_temp = temperature;              //atualiza temperatura atual
+
+       valorTemp = (int) temperature; 
+     
+      Serial.println(valorTemp);
+
+   } //end if
+
+  delay(80);
+  
+}//fim void temp
