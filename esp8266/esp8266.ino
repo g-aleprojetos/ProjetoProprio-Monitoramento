cab7de07a76f51b75bc3f0a19730653be56e101a
g-aleprojetos <g.aleprojetos@gmail.com>
2021-05-13 17:55:42 -0300

    configurar arquivo Json

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -15,15 +15,21 @@
 #include <FS.h>
 #include <ArduinoJson.h>
 
- 
+ // --- Configuração da rede e dispositivo ---
 const char* ssid = "g.aleprojetos";
 const char* password = "a248314b";
-String buf;
- 
 WiFiServer server(80);
  
+ // --- Protótipo das Funções ---
+void openFS();
+void createFile();
+void writeFile(String msg);
+void readFile(void);
+
+//--- Variáveis Globais ---
+String buf;
  
-void setup(void){
+void setup(){
   //Configura a porta serial para 115200bps
   Serial.begin(115200);
  
@@ -51,7 +57,7 @@ void setup(void){
   writeFile("Web Server started");
 }
  
-void loop(void){
+void loop(){
  
   //Tratamento das requisições http
   WiFiClient client = server.available();
@@ -81,5 +87,64 @@ void loop(void){
   client.print(buf);
   client.flush();
  
-  Serial.println("Client disonnected");
+  Serial.println("Client disconnected");
+}
+
+
+// Funções de Configuração ------------------------------
+
+void openFS(){
+  //Abre o sistema de arquivos
+  if(!SPIFFS.begin()){
+    Serial.println("Erro ao abrir o sistema de arquivos");
+  } else {
+    Serial.println("Sistema de arquivos aberto com sucesso!");
+  }
+}
+
+void createFile(){
+  File wFile;
+ 
+  //Cria o arquivo se ele não existir
+    if(SPIFFS.exists("/log.txt")){
+    Serial.println("Arquivo ja existe!");
+    } else {
+    Serial.println("Criando o arquivo...");
+    wFile = SPIFFS.open("/log.txt","w+");
+ 
+    //Verifica a criação do arquivo
+    if(!wFile){
+      Serial.println("Erro ao criar arquivo!");
+    } else {
+      Serial.println("Arquivo criado com sucesso!");
+    }
+  }
+  wFile.close();
+}
+
+void writeFile(String msg) {
+ 
+  //Abre o arquivo para adição (append)
+  //Inclue sempre a escrita na ultima linha do arquivo
+  File rFile = SPIFFS.open("/log.txt","a+");
+ 
+  if(!rFile){
+    Serial.println("Erro ao abrir arquivo!");
+  } else {
+    rFile.println("Log: " + msg);
+    Serial.println(msg);
+  }
+  rFile.close();
+}
+
+void readFile() {
+  //Faz a leitura do arquivo
+  File rFile = SPIFFS.open("/log.txt","r");
+  Serial.println("Reading file...");
+  while(rFile.available()) {
+    String line = rFile.readStringUntil('\n');
+    buf += line;
+    buf += "<br />";
+  }
+  rFile.close();
 }
