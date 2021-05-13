77be2ba50967daca618ad7f2ab49a3317d1907ca
g-aleprojetos <g.aleprojetos@gmail.com>
2021-05-13 19:58:35 -0300

    Update esp8266.ino
    Não foi possivel a gravação do login e senha. Mudar a configuração da senha para não mostrar.

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -15,83 +15,179 @@
 #include <FS.h>
 #include <ArduinoJson.h>
 
- // --- Configuração da rede e dispositivo ---
-const char* ssid = "g.aleprojetos";
-const char* password = "a248314b";
-WiFiServer server(80);
+// --- Configuração da rede e dispositivo ---
+const byte        WEBSERVER_PORT          = 80;                 // Porta Servidor Web
+const char*       WEBSERVER_HEADER_KEYS[] = {"User-Agent"};     // Headers do Servidor Web
+const byte        DNSSERVER_PORT          = 53;                 // Porta Servidor DNS
+const byte        LED_PIN                 = LED_BUILTIN;        // ESP8266 possui pino padrão de LED
+String            apWifi                  = "Wifi Automação";   // Nome do Wifi
+
+//--- Tamanho do Objeto JSON ---
+const   size_t    JSON_SIZE               = JSON_OBJECT_SIZE(5) + 600;
+
+//--- Instâncias --- 
+ESP8266WebServer  server(WEBSERVER_PORT);                         // Web Server
+DNSServer         dnsServer;                                      // DNS Server
+IPAddress         apIP(192, 168, 4, 1);
  
  // --- Protótipo das Funções ---
+void formatFS();
+void deleteFile();
 void openFS();
 void createFile();
-void writeFile(String msg);
-void readFile(void);
+boolean configRead();
+void  configReset();
+void readFile();
+boolean configSave();
+String deviceID();
 
 //--- Variáveis Globais ---
+char              login[30];    // login
+char              senha[30];    // senha
+char                 id[30];    // Identificação do dispositivo
+char               ssid[30];    // Rede WiFi
+char                 pw[30];    // Senha da Rede WiFi
 String buf;
  
 void setup(){
   //Configura a porta serial para 115200bps
-  Serial.begin(115200);
- 
-  //Abre o sistema de arquivos (mount)
+  Serial.begin(9600);
+  Serial.println("Serial configurado");
+
+   //Abre o sistema de arquivos (mount)
   openFS();
+  
+  //formatFS();
+  //deleteFile();
+ 
   //Cria o arquivo caso o mesmo não exista
   createFile();
+  //lê o arquivo de configuração
+  configRead();
+
+  pinMode(LED_PIN, OUTPUT);     // LED
+  digitalWrite(LED_PIN, HIGH); 
  
-  writeFile("Booting ESP8266...");
-  writeFile("Connecting to " + (String)ssid);
+  //--- WiFi Station ---
  
-  //Inicia a conexão WiFi
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pw);
+  Serial.println("Conectando WiFi ");
+  int b = 31;
+  while(WiFi.status() != WL_CONNECTED && b > 0) {
+    b--;
+    Serial.print(b);
+    Serial.print(" ");
     delay(500);
-    Serial.print(".");
   }
- 
-  writeFile("WiFi connected");
-  Serial.println(WiFi.localIP());
- 
-  //Inicia o webserver
+  Serial.println();
+
+  if (WiFi.status() == WL_CONNECTED) {
+    
+    // WiFi Station conectado
+    Serial.println("WiFi conectado");
+
+  } else {
+    
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP( apWifi );
+
+     //--- Habilita roteamento DNS ---
+  dnsServer.start(DNSSERVER_PORT , "*", apIP);
+  dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
+  server.on(F("/configInit")    , handleConfigInit);
+  server.on(F("/configSave"), handleConfigSave);
+  server.on(F("/css")       , handleCSS);
+  server.onNotFound(handleHomeInit);
+  server.collectHeaders(WEBSERVER_HEADER_KEYS, 1);
+  Serial.println("Entrou em configurar server\n ");
+
   server.begin();
-  writeFile("Web Server started");
+  }
 }
  
 void loop(){
- 
-  //Tratamento das requisições http
-  WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
- 
-  Serial.println("new client");
- 
-  while(!client.available()){
-    delay(1);
-  }
- 
-  String req = client.readStringUntil('\r');
-  Serial.println(req);
-  client.flush();
- 
-  buf = "";
- 
-  buf += "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n";
-  buf += "<h3 style=""text-align: center;"">ESP8266 Web Log</h3>";
-  buf += "<p>";
-  readFile();
-  buf += "</p>";
-  buf += "</html>\n";
- 
-  client.print(buf);
-  client.flush();
- 
-  Serial.println("Client disconnected");
+  yield();                            // WatchDog
+  dnsServer.processNextRequest();     // DNS 
+  server.handleClient();              // Web 
+
 }
 
+// Funções Genéricas ------------------------------------
+void log(String s) {
+  // Gera log na Serial
+  Serial.println(s);
+}
+
+String softwareStr() {
+  // Retorna nome do software
+  return String(__FILE__).substring(String(__FILE__).lastIndexOf("\\") + 1);
+}
+
+String longTimeStr(const time_t &t){
+  // Retorna segundos como "d:hh:mm:ss"
+  String s = String(t / SECS_PER_DAY) + ':';
+  if (hour(t) < 10) {
+    s += '0';
+  }
+  s += String(hour(t)) + ':';
+  if (minute(t) < 10) {
+    s += '0';
+  }
+  s += String(minute(t)) + ':';
+  if (second(t) < 10) {
+    s += '0';
+  }
+  s += String(second(t));
+  return s;
+}
+
+String hexStr(const unsigned long &h, const byte &l = 8) {
+  // Retorna valor em formato hexadecimal
+  String s;
+  s= String(h, HEX);
+  s.toUpperCase();
+  s = ("00000000" + s).substring(s.length() + 8 - l);
+  return s;
+}
+
+String deviceID() {
+  // Retorna ID padrão
+  #ifdef ESP8266
+    // ESP8266 utiliza função getChipId()
+    return "IeC-" + hexStr(ESP.getChipId());
+  #else
+    // ESP32 utiliza função getEfuseMac()
+    return "IeC-" + hexStr(ESP.getEfuseMac());
+  #endif
+}
+
+String ipStr(const IPAddress &ip) {
+  // Retorna IPAddress em formato "n.n.n.n"
+  String sFn = "";
+  for (byte bFn = 0; bFn < 3; bFn++) {
+    sFn += String((ip >> (8 * bFn)) & 0xFF) + ".";
+  }
+  sFn += String(((ip >> 8 * 3)) & 0xFF);
+  return sFn;
+}
 
 // Funções de Configuração ------------------------------
+void formatFS(){
+  //Apaga todos os arquivos na região de memória reservada
+  SPIFFS.format();
+    Serial.println("Apagados todos os arquivos na região de memória reservada");
+}
+
+void deleteFile(void) {
+  //Remove o arquivo
+  if(SPIFFS.remove("/ConfigInit.json")){
+    Serial.println("Arquivo removido com sucesso!");
+  } else {
+    Serial.println("Erro ao remover arquivo!");
+  }
+}
 
 void openFS(){
   //Abre o sistema de arquivos
@@ -106,11 +202,11 @@ void createFile(){
   File wFile;
  
   //Cria o arquivo se ele não existir
-    if(SPIFFS.exists("/log.txt")){
+    if(SPIFFS.exists("/ConfigInit.json")){
     Serial.println("Arquivo ja existe!");
     } else {
     Serial.println("Criando o arquivo...");
-    wFile = SPIFFS.open("/log.txt","w+");
+    wFile = SPIFFS.open("/ConfigInit.json","w+");
  
     //Verifica a criação do arquivo
     if(!wFile){
@@ -122,29 +218,172 @@ void createFile(){
   wFile.close();
 }
 
-void writeFile(String msg) {
- 
-  //Abre o arquivo para adição (append)
-  //Inclue sempre a escrita na ultima linha do arquivo
-  File rFile = SPIFFS.open("/log.txt","a+");
- 
-  if(!rFile){
-    Serial.println("Erro ao abrir arquivo!");
+boolean configRead() {
+  // Lê configuração
+  StaticJsonDocument<256> jsonConfigInit;
+
+  File file = SPIFFS.open(F("/ConfigInit.json"), "r");
+  if (deserializeJson(jsonConfigInit, file)) {
+    // Falha na leitura, assume valores padrão
+    configReset();
+
+    log(F("Falha lendo ConfigInit, assumindo valores padrão."));
+
+    // Salva configuração
+    configSave();   
+    return false;
   } else {
-    rFile.println("Log: " + msg);
-    Serial.println(msg);
+    // Sucesso na leitura
+    // Sucesso na leitura
+     strlcpy(login, jsonConfigInit["login"]        | "", sizeof(login));
+     strlcpy(senha,  jsonConfigInit["senha"]       | "", sizeof(senha));
+     strlcpy(ssid, jsonConfigInit["ssid"]          | "", sizeof(ssid)); 
+     strlcpy(pw, jsonConfigInit["pw"]              | "", sizeof(pw)); 
+
+    file.close();
+
+    Serial.println("\nLendo config:");
+    serializeJsonPretty(jsonConfigInit, Serial);
+    Serial.println("");
+
+    return true;
   }
-  rFile.close();
 }
 
-void readFile() {
-  //Faz a leitura do arquivo
-  File rFile = SPIFFS.open("/log.txt","r");
-  Serial.println("Reading file...");
-  while(rFile.available()) {
-    String line = rFile.readStringUntil('\n');
-    buf += line;
-    buf += "<br />";
+void  configReset() {
+  // Define configuração padrão
+ strlcpy(login, "admin", sizeof(login));
+ strlcpy(senha, "admin", sizeof(senha));
+ strlcpy(ssid, "", sizeof(ssid)); 
+ strlcpy(pw, "", sizeof(pw)); 
+  
+}
+
+boolean configSave() {
+  // Grava configuração
+   StaticJsonDocument<256> jsonConfigInit;
+
+  File file = SPIFFS.open(F("/ConfigInit.json"), "w+");
+  if (file) {
+    // Atribui valores ao JSON e grava
+    jsonConfigInit["login"]    = login;
+    jsonConfigInit["senha"]    = senha;
+    jsonConfigInit["ssid"]     = ssid;
+    jsonConfigInit["pw"]       = pw;
+  
+    serializeJsonPretty(jsonConfigInit, file);
+    file.close();
+
+    log(F("\nGravando config:"));
+    serializeJsonPretty(jsonConfigInit, Serial);
+    log("");
+
+    return true;
   }
-  rFile.close();
+  return false;
+}
+
+// Requisições Web --------------------------------------
+void handleHomeInit() {
+  // Home
+  File file = SPIFFS.open(F("/ConfigInit.html"), "r");
+  if (file) {
+    file.setTimeout(100);
+    String s = file.readString();
+    file.close();
+
+    // Atualiza conteúdo dinâmico
+    s.replace(F("#login#")     , login);
+    s.replace(F("#senha#")     , senha);
+    s.replace(F("#ssid#")      , ssid);
+      
+    // Envia dados
+    server.send(200, F("text/html"), s);
+    log("Config Init - Cliente: " + ipStr(server.client().remoteIP()) +
+        (server.uri() != "/" ? " [" + server.uri() + "]" : ""));
+  } else {
+    server.send(500, F("text/plain"), F("Home - ERROR 500"));
+    log(F("Config Init - ERRO lendo arquivo"));
+  }
+}
+
+void handleConfigInit() {
+  // Config
+  File file = SPIFFS.open(F("/ConfigInit.html"), "r");
+  if (file) {
+    file.setTimeout(100);
+    String s = file.readString();
+    file.close();
+
+    // Atualiza conteúdo dinâmico
+    s.replace(F("#login#")     , login);
+    s.replace(F("#senha#")     , senha);
+    s.replace(F("#ssid#")      , ssid);
+
+    // Send data
+    server.send(200, F("text/html"), s);
+    log("Config - Cliente: " + ipStr(server.client().remoteIP()));
+  } else {
+    server.send(500, F("text/plain"), F("Config - ERROR 500"));
+    log(F("ConfigInit - ERRO lendo arquivo"));
+  }
+}
+
+void handleConfigSave() {
+  // Grava Config
+  // Verifica número de campos recebidos
+  // Gera o campo adicional "plain" via post
+  if (server.args() == 5) {
+
+    String s;
+
+    // Grava login
+    s = server.arg("login");
+    s.trim();
+    strlcpy(login, s.c_str(), sizeof(login));
+
+    // Grava senha
+    s = server.arg("senha");
+    s.trim();
+    strlcpy(senha, s.c_str(), sizeof(senha));
+
+    // Grava ssid
+    s = server.arg("ssid");
+    s.trim();
+    strlcpy(ssid, s.c_str(), sizeof(ssid));
+
+    // Grava pw
+    s = server.arg("pw");
+    s.trim();
+    if (s != "") {
+      // Atualiza senha, se informada
+      strlcpy(pw, s.c_str(), sizeof(pw));
+    }
+
+    // Grava configuração
+    if (configSave()) {
+      server.send(200, F("text/html"), F("<html><meta charset='UTF-8'><script>alert('Configuração salva.');history.back()</script></html>"));
+      log("ConfigSave - Cliente: " + ipStr(server.client().remoteIP()));
+    } else {
+      server.send(200, F("text/html"), F("<html><meta charset='UTF-8'><script>alert('Falha salvando configuração.');history.back()</script></html>"));
+      log(F("ConfigSave - ERRO salvando Config"));
+    }
+  } else {
+    server.send(200, F("text/html"), F("<html><meta charset='UTF-8'><script>alert('Erro de parâmetros.');history.back()</script></html>"));
+  }
+}
+
+void handleCSS() {
+  // Arquivo CSS
+  File file = SPIFFS.open(F("/Style.css"), "r");
+  if (file) {
+    // Define cache para 3 dias
+    server.sendHeader(F("Cache-Control"), F("public, max-age=172800"));
+    server.streamFile(file, F("text/css"));
+    file.close();
+    log("CSS - Cliente: " + ipStr(server.client().remoteIP()));
+  } else {
+    server.send(500, F("text/plain"), F("CSS - ERROR 500"));
+    log(F("CSS - ERRO lendo arquivo"));
+  }
 }
