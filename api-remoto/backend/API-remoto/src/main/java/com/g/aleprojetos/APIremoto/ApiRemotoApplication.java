47948b51316fdf8b025c1692fcf44781bd9efde2
g-aleprojetos <g.aleprojetos@gmail.com>
2021-05-22 15:41:38 -0300

    Project created frontend / backend

---
+++ api-remoto/backend/API-remoto/src/main/java/com/g/aleprojetos/APIremoto/ApiRemotoApplication.java
@@ -0,0 +1,13 @@
+package com.g.aleprojetos.APIremoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRemotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRemotoApplication.class, args);
+	}
+
+}
---
+++ api-remoto/backend/API-remoto/src/test/java/com/g/aleprojetos/APIremoto/ApiRemotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g.aleprojetos.APIremoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRemotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
