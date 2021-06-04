c974e8f3adb3f3f9fd73dd8a5facb4c88a8a1a1d
g-aleprojetos <g.aleprojetos@gmail.com>
2021-06-04 16:14:59 -0300

    Create api java spring boot

---
+++ api-Servidor/backend/api-monitoramento-server/src/test/java/com/projetos/g/apimonitoramentoserver/ApiMonitoramentoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.g.apimonitoramentoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMonitoramentoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
