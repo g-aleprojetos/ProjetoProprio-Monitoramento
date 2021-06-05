d5e8c482f3d86f01d05a9b743b00a56f327abfb0
g-aleprojetos <g.aleprojetos@gmail.com>
2021-06-05 18:04:52 -0300

    Instalar swagger no api-monitoramento

--- api-Servidor/backend/api-monitoramento-server/src/main/java/com/projetos/g/apimonitoramentoserver/SpringFoxConfig.java
+++ api-Servidor/backend/api-monitoramento-server/src/main/java/com/projetos/g/apimonitoramentoserver/SpringFoxConfig.java
@@ -1,22 +1,21 @@
 package com.projetos.g.apimonitoramentoserver;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
-public class ApiMonitoramentoServerApplication {
+@Configuration
+public class SpringFoxConfig {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiMonitoramentoServerApplication.class, args);
-	}
-
-
-
+        @Bean
+        public Docket swagger() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
 }
--- api-Servidor/backend/api-monitoramento-server/src/main/java/com/projetos/g/apimonitoramentoserver/controllers/DeviceController.java
+++ api-Servidor/backend/api-monitoramento-server/src/main/java/com/projetos/g/apimonitoramentoserver/controllers/DeviceController.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,12 +18,12 @@ import java.util.List;
 public class DeviceController {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<DeviceDTO> save(@RequestBody DeviceDTO dto){
+    public ResponseEntity<DeviceDTO> save(@Valid @RequestBody DeviceDTO dto){
         return ResponseEntity.ok(dto);
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<DeviceDTO> update(@RequestBody DeviceDTO dto){
+    public ResponseEntity<DeviceDTO> update(@Valid @RequestBody DeviceDTO dto){
         return ResponseEntity.ok(dto);
     }
 
@@ -33,6 +36,10 @@ public class DeviceController {
         dto.setSerial("esp8266a");
         dto.setSoftware("temp 1.0");
         dto.setStatus(true);
+        dto.setDataCreated(LocalDate.now());
+        dto.setHourCreated(LocalTime.now());
+        dto.setDateFinished(null);
+        dto.setHourFinished(null);
         list.add(dto);
         return ResponseEntity.ok(list);
     }
@@ -40,19 +47,29 @@ public class DeviceController {
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<DeviceDTO>  findById(@PathVariable Long id){
         List<DeviceDTO> list = new ArrayList<>();
+
         DeviceDTO device1 = new DeviceDTO();
-        device1.setId(1L);
-        device1.setLocal("Piscina Climatizada");
-        device1.setSerial("esp8266a");
+        device1.setId(2L);
+        device1.setLocal("Câmera dos peixes");
+        device1.setSerial("esp8266b");
         device1.setSoftware("temp 1.0");
         device1.setStatus(true);
+        device1.setDataCreated(LocalDate.now());
+        device1.setHourCreated(LocalTime.now());
+        device1.setDateFinished(null);
+        device1.setHourFinished(null);
 
         DeviceDTO device2 = new DeviceDTO();
-        device2.setId(2L);
+        device2.setId(3L);
         device2.setLocal("Potaria de entrada");
-        device2.setSerial("esp8266b");
+        device2.setSerial("esp8266c");
         device2.setSoftware("ilum 1.2");
         device2.setStatus(true);
+        device2.setDataCreated(LocalDate.now());
+        device2.setHourCreated(LocalTime.now());
+        device2.setDateFinished(null);
+        device2.setHourFinished(null);
+
         list.add(device1);
         list.add(device2);
 
