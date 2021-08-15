045c74bbbe22af9243204055f7c93bf9c818f8cf
g-aleprojetos <g.aleprojetos@gmail.com>
2021-08-14 21:52:23 -0300

    projeto monitoramento java iniciado

---
+++ backend/Java/Monitoramento/src/test/java/com/g/aleprojetos/Monitoramento/MonitoramentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g.aleprojetos.Monitoramento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
