efe8f20c7faa917950007f32d574c28bfca80e17
Alexandre dos Santos Gonçalves <g.aleprojetos@gmail.com>
2021-11-12 08:17:58 -0300

    novo arquivo

---
+++ esp8266/react/monit01/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Login from './paginas/Login';
+
+function App() {
+  return (
+    <container>
+    <Login/>
+    </container>
+  );
+}
+
+export default App;
---
+++ esp8266/react/monit01/src/paginas/Login.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+
+
+
+const Login = () => {
+
+    return (
+        <container>
+            <h1>Brasil</h1>
+
+        </container>
+
+    )
+}
+
+export default Login
